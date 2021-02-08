97e12c2bd72e040e4056bb3c728a66c337c8c127
PabloLeth <lethlopez@hotmail.com>
2021-02-08 13:14:46 +0100

    creada estructura BD en MySQL

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,16 +21,16 @@ function Login() {
     
     return (
         <>
-            <div className="container" id="loginContainer">
+            <div className="container " id="loginContainer">
             <h1>Inicio de sesión</h1>
                 <form>
                     <div className="form-group form">
                         <label for="usernameLoginInput">Usuario</label>
-                        <input type="text" onChange={handleUser} className="form-control" placeholder="introduce el usuario" id="usernameLoginInput" />
+                        <input type="text" onChange={handleUser} className="form-control col-4" placeholder="introduce el usuario" id="usernameLoginInput" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group form ">
                         <label for="passwordLoginInput">Constraseña</label>
-                        <input type="password" onChange={handlePassword} className="form-control" placeholder="contraseña" id="passwordLoginInput" />
+                        <input type="password" onChange={handlePassword} className="form-control col-4" placeholder="contraseña" id="passwordLoginInput" />
                     </div>
                     <button className="btn btn-success " id="loginButton" onClick={login} >Iniciar sesión</button>
                   
