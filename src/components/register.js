01bcac2c829707490749667a41a338a90d669070
Pablo Leth <lethlopez@hotmail.com>
2021-02-08 20:15:08 +0100

    esquema DB actualizada

--- src/components/register.js
+++ src/components/register.js
@@ -1,17 +1,38 @@
 function Register() {
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+    const handleUser = (e) => {
+        setUser(e.target.value)
+    }
+
+
+    const login = (e) => {
+        e.preventDefault();
+              alert(`tu usuario ${user} y contraseña ${password} estan ahora en un useState`)
+
+    }
     return (
         <>
-            <h1>Registro</h1>
+            <h1>Registrar</h1>
            
             <div className="container" id="registerContainer">
                 <form>
                     <div className="form-group mt-4">
-                        <label for="usernameRegisterInput">Usuario</label>
+                        <label for="usernameRegisterInput">nombre</label>
                         <input type="text" className="form-control" placeholder="nombre" id="usernameRegisterInput" />
-                    </div>
+                    </div>  
                     <div className="form-group">
                         <label for="surnameRegisterInput">Apellidos</label>
                         <input type="text" className="form-control" placeholder="Apellidos" id="surnameRegisterInput" />
+                    </div>
+                    <div className="form-group mt-4">
+                        <label for="dniRegisterInput">Dni</label>
+                        <input type="text" className="form-control" placeholder="nombre" id="dniRegisterInput" />
                     </div>
                     <div className="form-group">
                         <label for="passwordRegisterInput">Constraseña</label>
@@ -20,6 +41,10 @@ function Register() {
                     <div className="form-group">
                         <label for="emailRegisterInput">email</label>
                         <input type="text" className="form-control" placeholder="email" id="emailRegisterInput" />
+                    </div>
+                    <div className="form-group mt-4">
+                        <label for="telephoneRegisterInput">telefono</label>
+                        <input type="text" className="form-control" placeholder="nombre" id="telephoneRegisterInput" />
                     </div>
                     <button className="btn btn-info" id="sendRegisterButton">Confirmar</button>
                 </form>
