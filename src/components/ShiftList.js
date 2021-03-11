7c721d1826cfcf25c3103bfd78b5ef8f0106bcb5
PabloLeth <lethlopez@hotmail.com>
2021-03-11 22:31:03 +0100

    pool update, mostrando datos desde el servidor

---
+++ src/components/ShiftList.js
@@ -0,0 +1,32 @@
+function ShiftList({data}) {
+ if (!data){
+   return <p>loading...</p>
+ }
+  return (
+    <>
+    {
+    data.map(({worker,branch, shiftType,startShift,endShift}) => {
+  
+      return(
+      <div className="card text-center cardsize">
+        <div className="card-body">
+          <h5 className="card-title">{worker}</h5>
+          <p className="card-text">{branch}</p>
+          <p className="card-text">{shiftType}</p>
+          
+
+          <p className="card-text">{startShift.date.slice(11,16)}</p>
+          <p className="card-text">{endShift.date.slice(11,16)}</p>
+   
+        
+          <a href="#" className="btn btn-primary">tomarlo</a>
+        </div>
+      </div>
+      )
+    })
+      }
+    </>
+  )
+}
+
+export default ShiftList;
--- src/pages/Pool.js
+++ src/pages/Pool.js
@@ -1,37 +1,46 @@
-import Card from '../components/Card';
+import ShiftList from '../components/ShiftList';
 import SearchShifts from '../components/SearchShifts';
 import { useEffect, useState } from 'react';
 import './pool.css';
 
 function Pool() {
- 
-  const [poolResults, setPoolResults] = useState([]);
+
+  const [{ data, error, isLoading }, dispatch] = useState({
+    data: null,
+    error: null,
+    isLoading: true
+  });
+
+
   const URLPOOL = "http://localhost/symfonyswapp/public/index.php/shift/pool";
+
   useEffect(() => {
     fetch(URLPOOL)
       .then(response => response.json())
-      .then(data => {
-        
+      .then(data => {dispatch({ data, error: null, isLoading: false })
+
         console.log(data);
-        setPoolResults(data);
+       }
 
-      }).catch(error => console.log("ha habido un error:", error));
+      ).catch(error => {
+        dispatch({ data: null, error, isLoading: false })
+        console.log("ha habido un error:", error)
+      });
 
 
-  },[]);
+  }, []);
 
+  // if (isLoading) return <Card />;
+  // if (error) return <Card error={error} />;
+  // // return <Card props={data} />
   return (
     <>
 
       <div className="container">
         <p>este es el Pool donde aparecen los turnos que se van a cambiar con otros usuarios</p>
         <SearchShifts />
-        {
-          poolResults.map( shift => {
-            console.log(shift);
-            return <Card props = {shift}/>
-          })
-        }
+        <ShiftList data={data} />
+         
       </div>
     </>
   );
