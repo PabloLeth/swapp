45a78b66b2d8e5f02e6d1791e4fe2511c49d60ba
PabloLeth <lethlopez@hotmail.com>
2021-03-13 14:55:38 +0100

    actualizado el archivo shiftList con pequeños cambios en el codigo

--- src/components/ShiftList.js
+++ src/components/ShiftList.js
@@ -5,7 +5,7 @@ function ShiftList({data}) {
   return (
     <>
     {
-    data.map(({worker,branch, shiftType,startShift,endShift}) => {
+    data.map(({worker,branch, shiftType,startShift,endShift,job}) => {
   
       return(
       <div className="card text-center cardsize">
@@ -13,12 +13,9 @@ function ShiftList({data}) {
           <h5 className="card-title">{worker}</h5>
           <p className="card-text">{branch}</p>
           <p className="card-text">{shiftType}</p>
-          
-
+          <p className="card-text">{job}</p>
           <p className="card-text">{startShift.date.slice(11,16)}</p>
           <p className="card-text">{endShift.date.slice(11,16)}</p>
-   
-        
           <a href="#" className="btn btn-primary">tomarlo</a>
         </div>
       </div>
@@ -28,5 +25,4 @@ function ShiftList({data}) {
     </>
   )
 }
-
 export default ShiftList;
