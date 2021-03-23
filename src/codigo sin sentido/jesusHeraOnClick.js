1bfc0905ef6741f8e08adc5025e6c7a37f593f09
PabloLeth <lethlopez@hotmail.com>
2021-03-23 17:14:33 +0100

    eliminado carpeta

--- src/codigo sin sentido/jesusHeraOnClick.js
+++
@@ -1,8 +0,0 @@
-<input type="submit" value="Submit" disabled={disable} className="prueba"
-          onClick={(e) => AuthService.putUser(e, id, companyNameChange, activityChange, regionChange, postalcodeChange, adressChange, token)
-            .then(((response) => {
-              setDisable(!disable);
-              
-          })).catch(err => {
-            console.log("Error at updating User, reason: ", err.info)
-          })}></input>
