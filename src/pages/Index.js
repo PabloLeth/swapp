c9f2d0b8613c2137d8fc8e74d68f2b2db816f388
PabloLeth <lethlopez@hotmail.com>
2021-03-31 00:22:53 +0200

    added css for index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,15 +1,23 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './index.css';
 function Indice() {
     return (
       <>
      
-      <div>
+        <div className="bg-pic">
+      <div className="container  " id="index-container">
+
        <h1>Bienvenidos a la experiencia de Swapp</h1>
        <h3>La herramienta perfecta tanto para ti como para tu equipo de trabajo. </h3>
-       <h6> ¿Cansado de que tu equipo este siempre a disgusto con los turnos de trabajo semanales que tanto trabajo te cuestan hacer?, ¿estas harta de que vengan a pedirte los cambios de turno y que tengas que hacer cambios y luego surjan los problemas? haz posible que puedan cambiar sus turnos de trabajo sin problemas, ofrece la libertad de poder hacer sus cambios entre ellos sin tener que acudir a ti. deja que nos encargemos nosotros de ese engorro. </h6>
+       <div className="container ">
+         <h6> ¿Cansado de que tu equipo este siempre a disgusto con los turnos de trabajo semanales que tanto trabajo te cuestan hacer?, ¿estas harta de que vengan a pedirte los cambios de turno y que tengas que hacer cambios y luego surjan los problemas? haz posible que puedan cambiar sus turnos de trabajo sin problemas, ofrece la libertad de poder hacer sus cambios entre ellos sin tener que acudir a ti. deja que nos encargemos nosotros de ese engorro. </h6>
+       </div>
       
-       <Link to= "/login">empezamos?</Link>
+       <div className="row justify-content-center">
+         <Link to= "/login"> empezamos?</Link>
+       </div>
       </div>
+        </div>
   
       </>
     );
